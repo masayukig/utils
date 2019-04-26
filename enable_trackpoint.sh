deb7265c3a3edbe1b8636fd981e6a8aa805c9b1b
Masayuki Igawa <masayuki@igawa.io>
2019-04-26 11:59:13 -0400

    Add enable_trackpoint.sh

---
+++ enable_trackpoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo sh -c "echo -n none > /sys/devices/platform/i8042/serio1/drvctl"
+sudo sh -c "echo -n reconnect > /sys/devices/platform/i8042/serio1/drvctl"
+sudo rmmod psmouse
+sudo modprobe psmouse
+sleep 1
+synclient TapButton1=0 TapButton2=0 TapButton3=0
