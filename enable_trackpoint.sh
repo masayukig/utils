e9c1e206d449d9f9e19507427d3c2bb65e83dc4f
Masayuki Igawa <masayuki@igawa.io>
2019-04-30 11:48:08 -0600

    Sleep more

--- enable_trackpoint.sh
+++ enable_trackpoint.sh
@@ -4,5 +4,7 @@ sudo sh -c "echo -n none > /sys/devices/platform/i8042/serio1/drvctl"
 sudo sh -c "echo -n reconnect > /sys/devices/platform/i8042/serio1/drvctl"
 #sudo rmmod psmouse
 sudo modprobe psmouse
-sleep 1
+sleep 3
 synclient TapButton1=0 TapButton2=0 TapButton3=0
+#synclient TapButton2=0
+#synclient TapButton3=0
