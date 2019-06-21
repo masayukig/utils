6b5af380a9c9c0582a29c03d841f08894408f37b
Masayuki Igawa <masayuki@igawa.io>
2019-06-21 15:15:54 +0900

    Implement authentication very roughly
    This commit adds authentication feature but it's still very rough yet.
    And it's still meaningless, though.

--- digest_wsj.sh
+++ digest_wsj.sh
@@ -1,10 +1,45 @@
 #!/usr/bin/env bash
 
+# https://stackoverflow.com/questions/44965524/scrap-articles-form-wsj-by-requests-curl-and-beautifulsoup
+
+. ${HOME}/.wsj_credentials
+
+login_url=$(curl -s -I "https://accounts.wsj.com/login")
+connection=$(echo "$login_url" | grep -oP "location:\s+.*connection=\K(\w+)")
+client_id=$(echo "$login_url" | grep -oP "location:\s+.*client_id=\K(\w+)")
+
+
+rm -f cookies.txt
+
+IFS='|' read -r wa wresult wctx < <(curl -s 'https://sso.accounts.dowjones.com/usernamepassword/login' \
+      --data-urlencode "username=$WSJ_USERNAME" \
+      --data-urlencode "password=$WSJ_PASSWORD" \
+      --data-urlencode "connection=$connection" \
+      --data-urlencode "client_id=$client_id" \
+      --data 'scope=openid+idp_id&tenant=sso&response_type=code&protocol=oauth2&redirect_uri=https%3A%2F%2Faccounts.wsj.com%2Fauth%2Fsso%2Flogin' | pup 'input json{}' | jq -r 'map(.value) | join("|")')
+
+# replace double quote ""
+wctx=$(echo "$wctx" | sed 's/&#34;/"/g')
+
+code_url=$(curl -D - -s -c cookies.txt 'https://sso.accounts.dowjones.com/login/callback' \
+     --data-urlencode "wa=$wa" \
+     --data-urlencode "wresult=$wresult" \
+     --data-urlencode "wctx=$wctx" | grep -oP "location:\s+\K(\S*)")
+
+curl -s -c cookies.txt "$code_url"
+
+# here call your URL loading cookies.txt
+#curl -s -b cookies.txt "https://www.wsj.com/articles/singapore-prime-minister-lee-rejects-claims-he-misused-state-powers-in-family-feud-1499094761?tesla=y"
+
+#exit
+
 WSJ_URL=$(expand_url.sh $1)
 ANSWER_URL=$(expand_url.sh $2)
 ANSWER_ID=$(basename -s .pdf ${ANSWER_URL})
 
-CONTENTS=$(curl -s ${WSJ_URL})
+#CONTENTS=$(curl -s ${WSJ_URL})
+CONTENTS=$(curl -s -b cookies.txt ${WSJ_URL})
+
 TITLE=$(echo ${CONTENTS} | xmllint --html --xpath "//h1/text()" - 2>/dev/null)
 DESCRIPTION=$(echo ${CONTENTS} | xmllint --html --xpath "(//h2)[1]/text()" - 2>/dev/null)
 
@@ -19,3 +54,5 @@ echo
 echo ${WSJ_URL}
 echo
 echo ${ANSWER_URL}
+
+echo ${CONTENTS} > ~/tmp/contents.html
