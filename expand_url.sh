f6ea0e9ca81192483e1792f534d8e1b1581ea276
Masayuki Igawa <masayuki@igawa.io>
2019-07-16 10:58:00 +0900

    Make case insensitive

--- expand_url.sh
+++ expand_url.sh
@@ -2,6 +2,6 @@
 
 for shorten_url in $1:
 do
-    curl -s -I ${shorten_url} | grep 'Location:' | cut -d' ' -f2 | tr -d '\r'
+    curl -s -I ${shorten_url} | grep -i 'Location:' | cut -d' ' -f2 | tr -d '\r'
 done
 
